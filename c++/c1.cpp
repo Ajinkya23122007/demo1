9334c34e4870fdead6342d4c677dfad817c27fc4
Ajinkya23122007 <ajinkyabhatkande@gmail.com>
2026-06-28 20:50:53 +0530

    c2..2

--- c++/c1.cpp
+++ c++/c1.cpp
@@ -12,3 +12,24 @@ int main(){
 }
 
 
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    int fact=1;
+    cout<<"this program find factorail of a given number";
+    cout<<"enter number"
+    cin>>n;
+    for (int i=0;i<n;i++);
+    fact=fact*i;
+
+cout<<"factorail of a given number is "<<fact;
+
+
+
+}
+
+
+
+
+
