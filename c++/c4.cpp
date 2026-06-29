4ea9446d9ff5dd557d76633d86d47d8074fd40a0
Ajinkya23122007 <ajinkyabhatkande@gmail.com>
2026-06-29 19:00:16 +0530

    pattern..5

--- c++/c4.cpp
+++ c++/c4.cpp
@@ -1,3 +1,28 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//    int n;
+//    cin>>n;
+
+//    int i=1;
+//    while(i<=n){
+//     int space;
+//     space=i-1;
+//     while(space){
+//         cout<<" ";
+//         space=space-1;
+//     }
+//     int j=1;
+//     while(j<=n-i+1){
+//         cout<<i;
+//         j=j+1;
+//     }
+//     cout<<endl;
+//     i=i+1;
+//    }
+// }
+
 #include<iostream>
 using namespace std;
 
@@ -15,7 +40,7 @@ int main(){
     }
     int j=1;
     while(j<=n-i+1){
-        cout<<i;
+        cout<<i+j-1;
         j=j+1;
     }
     cout<<endl;
