ab2a4e77a95c86aac5cfac3e705e3d13f0cb9755
Ajinkya23122007 <ajinkyabhatkande@gmail.com>
2026-06-29 19:05:07 +0530

    pattern..6

--- c++/c4.cpp
+++ c++/c4.cpp
@@ -1,27 +1,27 @@
-// #include<iostream>
-// using namespace std;
+#include<iostream>
+using namespace std;
 
-// int main(){
-//    int n;
-//    cin>>n;
+int main(){
+   int n;
+   cin>>n;
 
-//    int i=1;
-//    while(i<=n){
-//     int space;
-//     space=i-1;
-//     while(space){
-//         cout<<" ";
-//         space=space-1;
-//     }
-//     int j=1;
-//     while(j<=n-i+1){
-//         cout<<i;
-//         j=j+1;
-//     }
-//     cout<<endl;
-//     i=i+1;
-//    }
-// }
+   int i=1;
+   while(i<=n){
+    int space;
+    space=i-1;
+    while(space){
+        cout<<" ";
+        space=space-1;
+    }
+    int j=1;
+    while(j<=n-i+1){
+        cout<<i;
+        j=j+1;
+    }
+    cout<<endl;
+    i=i+1;
+   }
+}
 
 #include<iostream>
 using namespace std;
@@ -41,6 +41,33 @@ int main(){
     int j=1;
     while(j<=n-i+1){
         cout<<i+j-1;
+        j=j+1;
+    }
+    cout<<endl;
+    i=i+1;
+   }
+}
+
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+   int n;
+   cin>>n;
+
+   int i=1;
+   while(i<=n){
+    int space;
+    space=n-i;
+    while(space){
+        cout<<" ";
+        space=space-1;
+    }
+    int j=1;
+    while(j<=i){
+        cout<<i;
         j=j+1;
     }
     cout<<endl;
