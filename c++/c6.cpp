32d852453eec7d59180fdf8e736e45a37d0a4efd
Ajinkya23122007 <ajinkyabhatkande@gmail.com>
2026-06-30 13:26:11 +0530

    c6..4

--- c++/c6.cpp
+++ c++/c6.cpp
@@ -25,3 +25,38 @@ int main(){
     cout<<(--i)<<endl;
     //7,i=7
 }
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    bool isprime=1;
+    for(int i=2;i<n;i++){
+        if(n%i==0){
+            //cout<<" not a prime number"<<endl;
+            isprime=0;
+            break;
+        }
+    }
+    if(isprime==0)
+    {
+            cout<<"not a prime number";
+    }
+     else{
+            cout<<"is a prime number";
+     }
+
+ }
+
+
+ #include<iostream>
+ using namespace std;
+  int main(){
+    for (int i=0;i<5;i++){
+        for (int j=i;j<=5;j++){
+            cout<<i << " "<< j <<endl;
+        }
+    }
+  }
--- c++/tempCodeRunnerFile.cpp
+++ c++/tempCodeRunnerFile.cpp
@@ -1,1 +1,23 @@
-iostream
+#include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     bool isprime=1;
+//     for(int i=2;i<n;i++){
+//         if(n%i==0){
+//             //cout<<" not a prime number"<<endl;
+//             isprime=0;
+//             break;
+//         }
+//     }
+//     if(isprime==0)
+//     {
+//             cout<<"not a prime number";
+//     }
+//      else{
+//             cout<<"is a prime number";
+//      }
+
+//  }
