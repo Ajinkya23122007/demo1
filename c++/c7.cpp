7762d15b0dd6acde64a7f77d41995c0b70c01ad2
Ajinkya23122007 <ajinkyabhatkande@gmail.com>
2026-07-04 22:53:28 +0530

    c7..3

--- c++/c7.cpp
+++ c++/c7.cpp
@@ -32,3 +32,18 @@ else{
   cout<<"not a armstrong number";
 }
 }  
+
+
+#include<iostream>
+using namespace std;
+int main(){
+int n,temp,sum=0,digit;
+cin>>n;
+temp=n;
+while(n!=0){
+  digit=n%10;
+  sum=sum+digit;
+  n=n/10;
+}
+
+cout<<"sum of digit is"<<sum
