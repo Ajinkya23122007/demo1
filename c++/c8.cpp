1e5fa2c019ab73cbde34d2dc5c5fbb9d544105cc
Ajinkya23122007 <ajinkyabhatkande@gmail.com>
2026-07-05 16:49:34 +0530

    c8..1

---
+++ c++/c8.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+    int n,digit,ans=0,i=0;
+    cout<<"enter a binary number";
+    cin>>n;
+    while(n!=0){
+        digit=n%10;
+        if(digit==1){
+            ans=ans+pow(2,i);
+        }
+        n=n/10;
+        i++;
+
+    }
+    cout<<"the answer is "<<ans<<endl;
+}
