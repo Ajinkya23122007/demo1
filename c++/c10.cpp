ebda828c6aaddd4b4bf886576b7578425e4427ce
Ajinkya23122007 <ajinkyabhatkande@gmail.com>
2026-07-07 23:23:27 +0530

    c10..1

---
+++ c++/c10.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+void printarray(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i];
+    }
+}
+
+void sortarr(int arr[],int n){
+    int i=0;
+    int j=n-1;
+    while(i<=j){
+        while(arr[i]==0 && i<=j){
+            i++;
+
+        }
+        while(arr[j]==1 && i<=j){
+            j--;
+        }
+        while(arr[i]==1 && arr[j]==0 && i<=j){
+            swap(arr[i],arr[j]);
+            i++;
+            j--;
+        }
+
+    }
+}
