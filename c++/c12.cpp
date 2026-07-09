1a4a538ff2b3ad0d161060fc5e54aeb038d40cff
Ajinkya23122007 <ajinkyabhatkande@gmail.com>
2026-07-09 19:22:51 +0530

    c12..2

--- c++/c12.cpp
+++ c++/c12.cpp
@@ -50,3 +50,51 @@ int main(){
 
 
 
+
+
+#include<iostream>
+using namespace std;
+bool ispossible(arr[],int x,int n,int mid){
+    int stdentcount=0;
+    int pagesum=0;
+    for(int i-0;i<n;i++){
+        if(pagesum+arr[i]<=mid){
+            pagesum=pagesum+arr[i];
+        }
+        else{
+            studentcount++;
+            if(student>x || arr[i]>mid){
+                return false;
+            }
+            pagesum=arr[i];
+
+        }
+    }
+    return true;
+}
+
+int main(){
+    int arr[]={10,20,30,40}
+    int x=2;
+    int n=4;
+    int sum=0;
+    int s=0,unit=-1;
+    for(int i=0;i<n,i++){
+        sum=summ+arr[i];
+    }
+    int e=sum;
+    int mid = s+(e-s)/2;
+    while(s<=e){
+        bool result=ispossible(arr,x,n,mid)
+        if(result){
+            unit=mid;
+            s=mid+1;
+        }
+        else{
+            e=mid-1;
+        }
+        mid =s+(e-s)/2;
+    }
+    return ans;
+}
+
