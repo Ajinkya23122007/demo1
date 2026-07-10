7cc6e0aa8062f50b4a368ad4eea3d0d56cc5be9b
Ajinkya23122007 <ajinkyabhatkande@gmail.com>
2026-07-10 19:11:34 +0530

    c13..1

---
+++ c++/c13.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int sqrtbinarysearch(int n){
+    int s =0;
+    int ans=-1;
+    int e=n;
+    long long int mid =s+(e-s)/2;
+   while(s<=e){
+    long long int square=mid*mid;
+    if(square==n){
+        return mid;
+    }
+    if(square<n){
+        ans=mid;
+        s=mid+1;
+
+    }
+    else{
+        e=mid-1;
+    }
+    
+     mid =s+(e-s)/2;
+   }
+   return ans;
+}
+
