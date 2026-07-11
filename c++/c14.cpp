88c5b9db13f3131f123ff31b41d2b62091f75af0
Ajinkya23122007 <ajinkyabhatkande@gmail.com>
2026-07-11 21:23:14 +0530

    c14..1

---
+++ c++/c14.cpp
@@ -0,0 +1,19 @@
+//selection sort
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5]={2,0,4,1,6};
+    int n;
+    int ans;
+    cout<<"enter a size"<<endl;
+    cin >> n;
+    for(int i=0;i<n-1;i++){
+        int minindex=i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[minindex]){
+                minindex=j;
+            }
+        }
+        swap(arr[minindex],arr[i]);
+    }    
+}
