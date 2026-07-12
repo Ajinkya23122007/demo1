e7b7ab40a1fe2a0bb74728b216ffc15cc78a8196
Ajinkya23122007 <ajinkyabhatkande@gmail.com>
2026-07-12 17:29:23 +0530

    c14..2

--- c++/c14.cpp
+++ c++/c14.cpp
@@ -17,3 +17,28 @@ int main(){
         swap(arr[minindex],arr[i]);
     }    
 }
+
+//bubble sort 
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5]={2,0,4,1,6};
+    int n;
+    cout<<"enter a size"<<endl;
+    cin>>n;
+    for(int i=1;i<n;i++){
+        for(int j=0;j<n-i;j++)
+        {
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+
+            }
+
+        }
+    }
+    for (int i=0;i<n;i++){
+        cout<<arr[i]<<endl;
+    }
+    
+
+}
--- c++/tempCodeRunnerFile.cpp
+++ c++/tempCodeRunnerFile.cpp
@@ -1,33 +1,18 @@
-
-// #include<iostream>
+#include<iostream>
 // using namespace std;
-
-
-// int peaksort(int arr[],int n){
-//     int start=0;
-//     int end=n-1;
-//     int mid=start+(end-start)/2;
-//     while(start<end){
-//         if(arr[mid]<arr[mid+1]){
-//             start=mid + 1;
-
-//         }
-//         else{
-//             end=mid;
-//         }
-//         mid=start+(end-start)/2;
-
-//     }
-//     return start;
-
-// }
 // int main(){
-//     int arr[5]={1,2,3,6,7}, n;
-//     cout <<"this program find peak elemtns in array"<<endl;
+//     int arr[5]={2,0,4,1,6};
+//     int n;
+//     int ans;
 //     cout<<"enter a size"<<endl;
-//     cin>>n;
-//     int prakindex=peaksort(arr,n);
-//     cout<<"the peak element is at index "<<prakindex<<endl;
-//     return 0;
-
+//     cin >> n;
+//     for(int i=0;i<n-1;i++){
+//         int minindex=i;
+//         for(int j=i+1;j<n;j++){
+//             if(arr[j]<arr[minindex]){
+//                 minindex=j;
+//             }
+//         }
+//         swap(arr[minindex],arr[i]);
+//     }    
 // }
