916435be3189c1f67d0ebda6cd530d9a84e22135
Ajinkya23122007 <ajinkyabhatkande@gmail.com>
2026-07-15 18:43:21 +0530

    program to check if a string is a paildrome or not using two pointers approach

--- c++/c16.cpp
+++ c++/c16.cpp
@@ -14,4 +14,25 @@ public:
 // program to check if a string is a paildrome or not using two pointers approach 
 // just wrting logic
 #include<iostream>
-using name 
+using namespace std;
+int main(){
+    char name[20];
+    int n;
+    cout<<"enter a size"<<endl;
+    cin>>n;
+    cout<<"enter your word"<<endl;
+    cin>>name;
+    int s=0;
+    int e=n-1;
+    while(s<=e){
+        if(name[s]!=name[e]){
+            cout<<"not a palindeome"<<endl;
+            break;
+        }
+        else{
+            s++;
+            e--;
+        }
+        cout<<"its a palindrome"<<endl;
+    }
+}
--- c++/tempCodeRunnerFile.cpp
+++ c++/tempCodeRunnerFile.cpp
@@ -1,47 +1,12 @@
-//selection sort
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int arr[5]={2,0,4,1,6};
-//     int n;
-//     cout<<"enter a size"<<endl;
-//     cin >> n;
-//     for(int i=0;i<n-1;i++){
-//         int minindex=i;
-//         for(int j=i+1;j<n;j++){
-//             if(arr[j]<arr[minindex]){
-//                 minindex=j;
-//             }
-//         }
-//         swap(arr[minindex],arr[i]);
-//     }    
-//     for (int i=0;i<n;i++){
-//         cout<<arr[i]<<endl;
+class Solution {
+// public:
+//     void reverseString(vector<char>& s) {
+//         int st=0;
+//         int e=s.size()-1;
+//         while(st<e){
+//             swap(s[st],s[e]);
+//             st++;
+//             e--;
+//         }  
 //     }
-
-// }
-
-// //bubble sort 
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int arr[5]={2,0,4,1,6};
-//     int n;
-//     cout<<"enter a size"<<endl;
-//     cin>>n;
-//     for(int i=1;i<n;i++){
-//         for(int j=0;j<n-i;j++)
-//         {
-//             if(arr[j]>arr[j+1]){
-//                 swap(arr[j],arr[j+1]);
-
-//             }
-
-//         }
-//     }
-//     for (int i=0;i<n;i++){
-//         cout<<arr[i]<<endl;
-//     }
-    
-
-// }
+// };
