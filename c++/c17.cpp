daff1ba9c280976c295955a0e732129ff91e9b04
Ajinkya23122007 <ajinkyabhatkande@gmail.com>
2026-07-16 16:24:38 +0530

    valid palindrome

---
+++ c++/c17.cpp
@@ -0,0 +1,49 @@
+class solution{
+private:
+    bool valid(char ch){
+        if( ( =ch >='a' &&ch <='z') || (ch >='A'&& ch <="Z") || (ch >='0' && ch<='9') ){
+            return 1;
+        }
+        return 0;
+    }
+    char tolowercase(char ch){
+        if((ch >='a' && ch <='z') || (ch >='0' && ch <='9')){
+            return ch;
+        }
+        else{
+            char temp=ch-'A'+'a';
+            return temp;
+        }
+    }
+    bool checkpalindrome(string a){
+        int s=0;
+        int e=a.length(-1;
+        while(s<=e){
+            if(a[s]!=a[e]){
+                return 0;
+                break;
+            }
+            else{
+                s++;
+                e--;
+            }
+            return 1;
+        }
+    }
+}
+public:
+    bool isPalindrome(string s) {
+        string tenp="";
+        for(int i=0;i<s.length();i++){
+            if(valid(s[i])){
+                temp.push_back(s[i]);
+            }
+        }
+        for(int i=0;i<temp.length();i++){
+            temp[i]=tolowercase(temp[i])
+        }
+        return checkpalindrome(temp);
+
+        
+    }
+};
