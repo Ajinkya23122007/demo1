f60b91ac68df3e41185f3e720711bfa411cf98af
Ajinkya23122007 <ajinkyabhatkande@gmail.com>
2026-07-18 16:26:22 +0530

    search in 2d matrix

--- c++/c18.cpp
+++ c++/c18.cpp
@@ -15,3 +15,28 @@ string replacespaces(string &str){
     }
     retutn temp;
 }
+
+//search in a 2d matrix using binary search 
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row=matrix.size();
+        int col=matrix[0].size();
+
+        int rowindex=0;
+        int colindex=col-1;
+        while(rowindex<row && colindex>=0){
+            int element=matrix[rowindex][colindex];
+            if(element==target){
+                return 1;
+            }
+            if(element<target){
+                rowindex++;
+            }
+            else{
+                colindex--;
+            }
+        }
+        return 0;   
+    }
+};
