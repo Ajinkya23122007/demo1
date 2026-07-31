ae680d9afb1706815cb87da9c879062b7d8ad8de
Ajinkya23122007 <ajinkyabhatkande@gmail.com>
2026-07-31 17:16:12 +0530

    maximum pushes required to type words

--- c++/c20.cpp
+++ c++/c20.cpp
@@ -87,3 +87,28 @@ public:
         return ans ;
     }
 };
+
+
+//Minimum Number of Pushes to Type
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int n=word.length();
+        int sum=0;
+        for(int i=0;i<n;i++){
+            if(i<8){
+                sum=sum+1;
+            }
+            else if(i<16){
+                sum=sum+2;
+            }
+            else if(i<24){
+                sum=sum+3;
+            }
+            else{
+                sum=sum+4;
+            }
+        }
+        return sum;
+    }
+};
