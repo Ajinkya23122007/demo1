d80ca3266068ff1ba0f485800f60b08338a7c764
Ajinkya23122007 <ajinkyabhatkande@gmail.com>
2026-09-12 14:44:45 +0530

    checking if a ll is palindrome or not using extra space

---
+++ chechifalinkedlistisplindrome.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool checkpalindrome(vector<int> arr){
+        int n=arr.size();
+        int s=0;
+        int e=n-1;
+        while(s<=e){
+            if(arr[s]!=arr[e]){
+                return 0;
+            }
+            s++;
+            e--;
+        }
+        return 1;
+    }    
+public:
+    bool isPalindrome(ListNode* head) {
+        vector<int> arr;
+        ListNode* temp=head;
+        while(temp!=NULL){
+            arr.push_back(temp->val);
+
+        }
+        return checkpalindrome(arr);
+    }
+};
